8d00d1d8cc5b0d9c03c3ca9e3b5637f360f96c60
Violeta Saravia <saraviavioleta@gmail.com>
2021-06-19 02:14:20 -0300

    selenium test 2

--- higher_lower.py
+++ higher_lower.py
@@ -1,27 +1,41 @@
 """
 Día 14: Selenium Test
+
+Instagram ya no permite ver perfiles sin cuenta. Esto no funciona más. RIP :(
+(hasta que aprenda de OAuth al menos)
 """
 
-import selenium
+# import selenium
 from selenium import webdriver
-import pandas as pd
+from selenium.webdriver.chrome.options import Options
+# import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://www.instagram.com/hunterschafer/?hl=en')
+chrome_options = Options()
+chrome_options.add_argument('-headless')
+chrome_options.add_argument('-width=1920')
+chrome_options.add_argument('-height=1080')
 
-POSTS_XPATH = 'html/body/div[1]/section/main/div/header/section/ul/li[1]/a/span'
-SEGUIDORES_XPATH = 'html/body/div[1]/section/main/div/header/section/ul/li[2]/a/span'
-SIGUIENDO_XPATH = '/html/body/div[1]/section/main/div/header/section/ul/li[3]/a/span'
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
 
-Posts = driver.find_element_by_xpath(POSTS_XPATH).text
-Seguidores = driver.find_element_by_xpath(SEGUIDORES_XPATH).text
-Siguiendo = driver.find_element_by_xpath(SIGUIENDO_XPATH).text
+xpaths_ig = {}
+xpaths_ig['Posts'] = '/html/body/div[1]/section/main/div/header/section/ul/li[1]/a/span'
+xpaths_ig['Seguidores'] = '/html/body/div[1]/section/main/div/header/section/ul/li[2]/a/span'
+xpaths_ig['Siguiendo'] = '/html/body/div[1]/section/main/div/header/section/ul/li[3]/a/span'
 
-print(Posts)
-print(Seguidores)
-print(Siguiendo)
-# /html/body/div[1]/section/main/div/header/section/ul/li[1]/a/span
-# /html/body/div[1]/section/main/div/ul/li[2]/a/span
-# /html/body/div[1]/section/main/div/header/section/ul/li[1]/a/span
-# /html/body/div[1]/section/main/div/header/section/ul/li[3]/a/span
+Users = {}
+Users['Hunter Schafer'] = 'hunterschafer'
+
+def url_ig(user):
+    """ Convierte user en url de ig """
+    return 'https://www.instagram.com/{}/?hl=en'.format(user)
+
+def cargar(xpath):
+    """ carga xpath """
+    return driver.find_element_by_xpath(xpath).text
+
+for nombre, username in Users.items():
+    driver.get(url_ig(username))
+    # breakpoint()
+    for stat, valor in xpaths_ig.items():
+        print(cargar(valor))
